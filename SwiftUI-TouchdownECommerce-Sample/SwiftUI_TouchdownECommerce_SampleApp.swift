5a8ad3bf195c4ad2bef9110dddde429c0390f76a
ekayaint <ekayaint@gmail.com>
2023-08-13 16:12:12 +0300

    Initial Commit

---
+++ SwiftUI-TouchdownECommerce-Sample/SwiftUI_TouchdownECommerce_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TouchdownECommerce_SampleApp.swift
+//  SwiftUI-TouchdownECommerce-Sample
+//
+//  Created by ekayaint on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TouchdownECommerce_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
