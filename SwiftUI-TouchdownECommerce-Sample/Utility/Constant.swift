b5dc3147a0e5e78943937de0da5cdef194dacbe4
ekayaint <ekayaint@gmail.com>
2023-08-17 11:53:52 +0300

    Started

---
+++ SwiftUI-TouchdownECommerce-Sample/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  SwiftUI-TouchdownECommerce-Sample
+//
+//  Created by ekayaint on 17.08.2023.
+//
+
+import SwiftUI
+
+
+// DATA
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
